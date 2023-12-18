652ab9242dca0c48381a39456ecf42eeee5e8311
Isak <isak.drevstad10@gmail.com>
2023-12-18 20:43:45 +0100

    feat(ui): Color the graph based on each tier

--- src/plot.py
+++ src/plot.py
@@ -7,6 +7,34 @@ from matplotlib.ticker import FuncFormatter
 LOCAL_TIMEZONE = pytz.timezone("Europe/Stockholm")
 
 
+def color_ranks(thresholds: dict):
+    """
+    Colors the graph with each rank's color
+    """
+    RANK_COLORS = {
+        "IRON": "#b5a58b",
+        "BRONZE": "#8c523a",
+        "SILVER": "#84969b",
+        "GOLD": "#f0b753",
+        "PLATINUM": "#4a927c",
+        "EMERALD": "#48c750",
+        "DIAMOND": "#716bf6",  # TODO: find a better color?
+        "MASTER": "#ed5eba",
+        "GRANDMASTER": "#ce4039",
+        "CHALLENGER": "#40c0de",
+    }
+
+    unique_tiers = {item["tier"] for item in thresholds}
+    for tier in unique_tiers:
+        upper_bound = max(
+            item["maxValue"] for item in thresholds if item["tier"] == tier
+        )
+        lower_bound = min(
+            item["minValue"] for item in thresholds if item["tier"] == tier
+        )
+        plt.axhspan(lower_bound, upper_bound, facecolor=RANK_COLORS[tier], alpha=0.8)
+
+
 def value_to_rank(y, _, thresholds: dict, short=False) -> str:
     """
     Translates a value to a rank string. Set short=True to output a short string
@@ -16,7 +44,11 @@ def value_to_rank(y, _, thresholds: dict, short=False) -> str:
         return {"IV": 4, "III": 3, "II": 2, "I": 1}[s]
 
     for tier in thresholds:
-        if y >= tier["minValue"] and y < tier["maxValue"]:
+        if y >= tier["minValue"] and (
+            y <= tier["maxValue"]
+            and y - tier["minValue"]
+            != 100  # this check is needed to not include E1 100LP for example
+        ):
             lp = y - tier["minValue"] + tier["minLP"]
             lp_str = f" {int(lp)} LP" if short else ""
             if short:
@@ -48,7 +80,7 @@ def plot(summoner_name: str):
     print(y_values)
 
     fig, ax = plt.subplots()
-    (line,) = ax.plot(x_values, y_values)
+    (line,) = ax.plot(x_values, y_values, color="black")
 
     offset = 50  # offset amount of lp
     ax.set_ylim(min(y_values) - offset, max(y_values) + offset)
@@ -66,6 +98,8 @@ def plot(summoner_name: str):
         ),
     )
     fig.canvas.mpl_connect("motion_notify_event", crosshair.on_mouse_move)
+
+    color_ranks(data["thresholds"])  # type: ignore
 
     plt.title(f"Rank history - [{summoner_name}]")
     plt.xlabel("Games ago")
